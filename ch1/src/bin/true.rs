d98f9b81e7f514370c5ea6c31fb5bf0d7d9aca99
Matt Stone <matthew.stone12@gmail.com>
2023-01-19 11:56:19 -0800

    ch1

---
+++ ch1/src/bin/true.rs
@@ -0,0 +1,10 @@
+//
+// true.rs
+// Copyright (C) 2023 msto <matthew.stone12@gmail.com>
+// Distributed under terms of the MIT license.
+//
+
+
+fn main() {
+    std::process::exit(0);
+}
---
+++ ch1/tests/cli.rs
@@ -0,0 +1,30 @@
+//
+// cli.rs
+// Copyright (C) 2023 msto 
+// Distributed under terms of the MIT license.
+//
+
+use assert_cmd::Command;
+
+
+#[test]
+fn runs() {
+    // let mut cmd = Command::new("hello");
+    // let res = cmd.output();
+    // assert!(res.is_ok());
+
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success().stdout("Hello, world!\n");
+}
+
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
+}
